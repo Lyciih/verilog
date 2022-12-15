a90f06041b95013857aec484cd843a4b70ff3f56
Lyciih <gnehciyil@gmail.com>
2022-12-15 14:23:56 +0800

    test

--- four_adder/four_adder_tb.v
+++ four_adder/four_adder_tb.v
@@ -1,10 +1,11 @@
 module main;
 reg signed [3:0] a;
 reg signed [3:0] b;
+reg signed c;
 wire signed [3:0] sum;
 wire c_out;
 
-four_adder test(sum, c_out, 1'b0, a, b);
+four_adder test(sum, c_out, c, a, b);
 
 
 initial
@@ -13,16 +14,22 @@ begin
     $dumpvars(0,main);
     a = 4'b0000;
     b = 4'b0000;
-    repeat(16)begin
-        repeat(16)begin   
-            $monitor("%dns monitor: a=%b b=%b sum=%b", $stime, a, b, sum);
-            #2 b=b+1;
-        end
-        a=a+1;
-        b=0;
-    end
-
+    c = 0;
+    repeat(2)begin
+	    repeat(16)begin
+		    repeat(16)begin   
+			$monitor("%dns monitor: a=%b b=%b c=%b sum=%b", $stime, a, b, c, sum);
+			#2 b=b+1;
+			end
+		a=a+1;
+        	b=0;
+		end
+    	a = 4'b0000;
+    	b = 4'b0000;
+    	c = 1;
+	end
 end
+	
 
 
 
